ef08446addf6211452c39f036b5f37c61c76d0f1
aldojoss <168256201+aldojoss@users.noreply.github.com>
2025-11-17 17:44:57 -0600

    Add files via upload

---
+++ Querys/virtualbookspObtenerUsuarios.sql
@@ -0,0 +1,20 @@
+USE Db_VirtualBook;
+GO
+-- =============================================
+--procedimiento de obtener usuarios
+-- =============================================
+CREATE OR ALTER PROCEDURE sp_ObtenerUsuarios
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT 
+        u.IdUsuario,
+        u.Nombres,
+        u.Apellidos,
+        u.Correo_Electronico,
+        r.NombreRol,
+        u.FotoPerfil
+    FROM Usuario u
+    INNER JOIN Rol r ON u.IdRol = r.IdRol;
+END;
+GO
